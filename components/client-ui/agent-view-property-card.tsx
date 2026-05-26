ea59e80cdcf73d8d43f710e4446fe53bdd546d6b
wisdomfzolutions <wisdom@fzolutions.com>
2026-05-26 19:10:34 +0100

    single product fetch

--- components/client-ui/agent-view-property-card.tsx
+++ components/client-ui/agent-view-property-card.tsx
@@ -8,10 +8,12 @@ const PropertyCard = ({ item }: { item: any }) => {
   const router = useRouter();
 
   const handleNavigation = () => {
-    router.push({
-      pathname: "/(utilities)/[id]",
-      params: { id: item.id },
-    });
+  router.push({
+    pathname: "/(utilities)/property-view",
+    params: {
+      propertyId: item.id,
+    },
+  });
   };
 
   return (
