e63a69e9cfb2f3eac4560fad33bb23bf8830e595
wisdomfzolutions <wisdom@fzolutions.com>
2026-06-15 12:25:42 +0100

    background tracking fix

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import { auth } from "../config/firebase";
 
 // ✅ Create Axios instance
 export const API = axios.create({
-  baseURL: "https://dwellify-backend-bq39.onrender.com/api",
+  baseURL: "https://3fec-102-90-101-188.ngrok-free.app/api",
   timeout: 120000,
 });
 
