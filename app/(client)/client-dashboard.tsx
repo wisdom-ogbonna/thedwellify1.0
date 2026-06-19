25a9ac62dcda69f1295ccf6848808ea1f65af039
Thompson Daniel <thompsonacedaniel@gmail.com>
2026-06-19 10:04:53 +0100

    all remaining bugs have been fixed before launch on june 20th

--- app/(client)/client-dashboard.tsx
+++ app/(client)/client-dashboard.tsx
@@ -93,7 +93,7 @@ export default function RequestMatchScreen() {
 
     setTimeout(() => {
       ref.current?.scrollTo(SNAP_50);
-    }, 100);
+    }, 1000);
   }, [SNAP_50]);
 
   useEffect(() => {
