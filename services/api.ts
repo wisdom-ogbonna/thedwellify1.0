f3ce0b114ca270bcd4e29f1cb69aa766419aa642
Thompson Daniel <thompsonacedaniel@gmail.com>
2026-06-21 22:05:42 +0100

    android rv 1

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import { auth } from "../config/firebase";
 
 // ✅ Create Axios instance
 export const API = axios.create({
-  baseURL: "https://126f-102-90-98-216.ngrok-free.app/api",
+  baseURL: "https://dwellify-backend-bq39.onrender.com/api",
   timeout: 120000,
 });
 
